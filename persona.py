24001ef091052be9741820a52e3609bd7479a973
assia <abf70@alu.ua.es>
2026-02-23 13:50:56 +0100

    clase coche

--- persona.py
+++ persona.py
@@ -13,3 +13,26 @@ class Persona:
             cad += f', Su coche: {self.coche.matricula} ({self.coche.marca})'
 
         return cad
+
+    def vender_coche(self, a_persona):
+        #Comprobar que el vendedor tiene coche
+        if self.coche == None:
+            print('No tienes coche para vender...')
+            return False
+
+        #Combrobar que el comprador no tiene coche
+        if a_persona.coche != None:
+            print(f'{a_persona} ya tienes coche...')
+            return False
+
+        a_persona.coche = self.coche
+        self.coche = None
+
+        print(f'{self.nombre} ha vendido correctamente su coche a {a_persona.nombre}')
+        return True
+
+
+
+
+
+
