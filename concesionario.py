255d1b8ea52856270598dbb47f896c3aa661423a
assia <abf70@alu.ua.es>
2026-03-16 13:18:55 +0100

    clase concesionario.py

---
+++ concesionario.py
@@ -0,0 +1,9 @@
+#concesionario.py
+
+class Concesionario:
+    def __init__(self,nombre, lista_coches, lista_personas):
+        self.nombre = nombre
+        self.lista_coches = lista_coches
+        self.lista_personas = lista_personas
+
+        
